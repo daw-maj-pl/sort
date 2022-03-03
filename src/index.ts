8d2e33002c1d481e9d96e387064dd2aac1033277
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-03 18:08:43 +0100

    Implement linked list

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([12, 4, -6, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('YCEcZxpd');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('YCEcZxpd');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(300);
+linkedList.add(20);
+linkedList.add(-40);
+linkedList.add(-3);
+linkedList.add(15);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
