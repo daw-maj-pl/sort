68b7d2caaeee67dc809415779e29ef76cf9f8e9c
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-05 21:02:01 +0100

    Refactor CharactersCollection and LinkedList to extend Sorter class; Delete unneeded instances of Sorter

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,16 @@ const numbersCollection = new NumbersCollection([12, 4, -6, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('YCEcZxpd');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('YCEcZxpd');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(300);
-// linkedList.add(20);
-// linkedList.add(-40);
-// linkedList.add(-3);
-// linkedList.add(15);
+const linkedList = new LinkedList();
+linkedList.add(300);
+linkedList.add(20);
+linkedList.add(-40);
+linkedList.add(-3);
+linkedList.add(15);
 
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// linkedList.print();
+linkedList.sort();
+linkedList.print();
